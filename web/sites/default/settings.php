5ce33e6e16c719be109c2a602843a725ccb55d2e
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-04-09 13:01:13 +0300

    develop: change settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -872,8 +872,8 @@ if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
   include $app_root . '/' . $site_path . '/settings.lando.php';
 }
 
-# if(file_exists($app_root . '/' . $site_path . '/settings.production.php')) {
-#  include $app_root . '/' . $site_path . '/settings.production.php';
-# }
+if(file_exists($app_root . '/' . $site_path . '/settings.production.php')) {
+  include $app_root . '/' . $site_path . '/settings.production.php';
+}
 
 $settings['config_sync_directory'] = '../config/sync';
